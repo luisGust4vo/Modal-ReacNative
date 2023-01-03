4c217fb7023b75da14a39043ff8f98bbb326e2fc
Luis Gustavo <75265319+luisGust4vo@users.noreply.github.com>
2023-01-03 08:35:46 -0300

    Update App.js

--- modal/App.js
+++ modal/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button, Modal } from 'react-native';
+import { View, Text, StyleSheet, Button, Modal } from 'react-native';e
 
 import Detalhes from './src/Detalhes';
 
